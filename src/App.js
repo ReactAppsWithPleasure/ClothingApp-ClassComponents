2475808f71d895c5621372a6bc266845ad1a33b5
TanyaRan <tatyanaran@gmail.com>
2021-09-21 13:04:41 +0300

    adding header component with logo and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Switch, Route } from 'react-router-dom';
+
+import './app.scss';
+
 import Home from './pages/home/home';
 import Shop from './pages/shop/shop';
-import './app.scss';
+import Header from './components/header/header';
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path='/' component={Home} />
 				<Route path='/shop' component={Shop} />
