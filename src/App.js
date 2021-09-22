ba20332dd12f0ada78f703a6631bc9950471c23b
TanyaRan <tatyanaran@gmail.com>
2021-09-22 10:08:00 +0300

    adding sign-in-up page, custom button and form-input components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './app.scss';
 import Home from './pages/home/home';
 import Shop from './pages/shop/shop';
 import Header from './components/header/header';
+import SignToApp from './pages/sign-to-app/sign-to-app';
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 			<Switch>
 				<Route exact path='/' component={Home} />
 				<Route path='/shop' component={Shop} />
+				<Route path='/signin' component={SignToApp} />
 			</Switch>
 		</div>
 	);
---
+++ src/pages/sign-to-app/sign-to-app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in';
+import './sign-to-app.scss';
+
+const SignToApp = () => (
+	<div>
+		<SignIn />
+		<div>Sign Up</div>
+	</div>
+);
+
+export default SignToApp;
